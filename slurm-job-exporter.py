1373e756c9a5fbe6a35ccfed0c8ce4830cd2db00
Simon Guilbault <sigui4@xcat1.int.ets1.calculquebec.ca>
2021-03-05 21:04:10 +0000

    Allocating a port on the prometheus wiki

--- slurm-job-exporter.py
+++ slurm-job-exporter.py
@@ -148,8 +148,8 @@ if __name__ == '__main__':
     parser.add_argument(
         '--port',
         type=int,
-        default=8080,
-        help='Collector http port, default is 8080')
+        default=9798,
+        help='Collector http port, default is 9798')
     args = parser.parse_args()
 
     start_http_server(args.port)
