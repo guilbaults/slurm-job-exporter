d31eaca846668b306f08a103b3ce0f2aa5325394
Simon Guilbault <simon.guilbault@calculquebec.ca>
2023-05-16 15:24:49 +0000

    Adding go version, handling memory

---
+++ go.mod
@@ -0,0 +1,22 @@
+module slurm-job-exporter
+
+go 1.17
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/containerd/cgroups v1.1.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/godbus/dbus/v5 v5.0.4 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/opencontainers/runtime-spec v1.0.2 // indirect
+	github.com/prometheus/client_golang v1.15.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os/user"
+	"path/filepath"
+	"regexp"
+
+	"github.com/containerd/cgroups"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/procfs"
+)
+
+type cgroupCollector struct {
+	slurm_job_memory_usage         *prometheus.Desc
+	slurm_job_memory_max           *prometheus.Desc
+	slurm_job_memory_limit         *prometheus.Desc
+	slurm_job_memory_cache         *prometheus.Desc
+	slurm_job_memory_rss           *prometheus.Desc
+	slurm_job_memory_rss_huge      *prometheus.Desc
+	slurm_job_memory_mapped_file   *prometheus.Desc
+	slurm_job_memory_active_file   *prometheus.Desc
+	slurm_job_memory_inactive_file *prometheus.Desc
+	slurm_job_memory_unevictable   *prometheus.Desc
+}
+
+func newCgroupCollector() *cgroupCollector {
+	return &cgroupCollector{
+		slurm_job_memory_usage: prometheus.NewDesc("slurm_job_memory_usage",
+			"Memory used by a job",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_max: prometheus.NewDesc("slurm_job_memory_max",
+			"Maximum memory used by a job",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_limit: prometheus.NewDesc("slurm_job_memory_limit",
+			"Memory limit of a job",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_cache: prometheus.NewDesc("slurm_job_memory_cache",
+			"bytes of page cache memory",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_rss: prometheus.NewDesc("slurm_job_memory_rss",
+			"bytes of anonymous and swap cache memory (includes transparent hugepages).",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_rss_huge: prometheus.NewDesc("slurm_job_memory_rss_huge",
+			"bytes of anonymous transparent hugepages",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_mapped_file: prometheus.NewDesc("slurm_job_memory_mapped_file",
+			"bytes of mapped file (includes tmpfs/shmem)",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_active_file: prometheus.NewDesc("slurm_job_memory_active_file",
+			"bytes of file-backed memory on active LRU list",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_inactive_file: prometheus.NewDesc("slurm_job_memory_inactive_file",
+			"bytes of file-backed memory on inactive LRU list",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+		slurm_job_memory_unevictable: prometheus.NewDesc("slurm_job_memory_unevictable",
+			"bytes of memory that cannot be reclaimed (mlocked etc)",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
+	}
+}
+
+func (collector *cgroupCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.slurm_job_memory_usage
+	ch <- collector.slurm_job_memory_max
+	ch <- collector.slurm_job_memory_limit
+	ch <- collector.slurm_job_memory_cache
+	ch <- collector.slurm_job_memory_rss
+	ch <- collector.slurm_job_memory_rss_huge
+	ch <- collector.slurm_job_memory_mapped_file
+	ch <- collector.slurm_job_memory_active_file
+	ch <- collector.slurm_job_memory_inactive_file
+	ch <- collector.slurm_job_memory_unevictable
+}
+
+func (collector *cgroupCollector) Collect(ch chan<- prometheus.Metric) {
+	files, _ := filepath.Glob("/sys/fs/cgroup/memory/slurm/uid_*")
+	for _, file := range files {
+		uid := filepath.Base(file)[4:]
+		user, _ := user.LookupId(uid)
+
+		job_paths, _ := filepath.Glob("/sys/fs/cgroup/memory/slurm/uid_" + uid + "/job_*")
+		for _, job_path := range job_paths {
+			job_id := filepath.Base(job_path)[4:]
+
+			control, err := cgroups.Load(cgroups.V1, cgroups.StaticPath("/slurm/uid_"+uid+"/job_"+job_id))
+			if err != nil {
+				log.Fatal(err)
+			}
+			stats, err := control.Stat(cgroups.IgnoreNotExist)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			processes, err := control.Processes(cgroups.Devices, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var account string = ""
+			for _, process := range processes {
+				pid := process.Pid
+
+				proc, err := procfs.NewProc(pid)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				r_account, err := regexp.Compile("SLURM_JOB_ACCOUNT=(.+)")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				envs, _ := proc.Environ()
+				for _, env := range envs {
+					if r_account.MatchString(env) {
+						account = r_account.FindStringSubmatch(env)[1]
+						break
+					}
+				}
+			}
+
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_usage, prometheus.GaugeValue,
+				float64(stats.Memory.Usage.Usage),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_max, prometheus.GaugeValue,
+				float64(stats.Memory.Usage.Max),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_limit, prometheus.GaugeValue,
+				float64(stats.Memory.HierarchicalMemoryLimit),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_cache, prometheus.GaugeValue,
+				float64(stats.Memory.Cache),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_rss, prometheus.GaugeValue,
+				float64(stats.Memory.RSS),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_rss_huge, prometheus.GaugeValue,
+				float64(stats.Memory.RSSHuge),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_mapped_file, prometheus.GaugeValue,
+				float64(stats.Memory.MappedFile),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_active_file, prometheus.GaugeValue,
+				float64(stats.Memory.ActiveFile),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_inactive_file, prometheus.GaugeValue,
+				float64(stats.Memory.InactiveFile),
+				account, job_id, user.Username)
+			ch <- prometheus.MustNewConstMetric(collector.slurm_job_memory_unevictable, prometheus.GaugeValue,
+				float64(stats.Memory.Unevictable),
+				account, job_id, user.Username)
+		}
+	}
+}
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	r := prometheus.NewRegistry()
+	cgroups := newCgroupCollector()
+	r.MustRegister(cgroups)
+	handler := promhttp.HandlerFor(r, promhttp.HandlerOpts{})
+	http.Handle("/metrics", handler)
+	http.ListenAndServe(":9101", nil)
+}
