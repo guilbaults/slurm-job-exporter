664cb8235ce94193deec081ca2f2ddbe6aeccd65
Simon Guilbault <simon.guilbault@calculquebec.ca>
2023-05-17 20:18:07 +0000

    Trying to use dcgm.GetLatestValuesForFields, but it does not return the correct values

--- go.mod
+++ go.mod
@@ -3,11 +3,19 @@ module slurm-job-exporter
 go 1.17
 
 require (
+	github.com/NVIDIA/go-dcgm v0.0.0-20230510130233-fc4b46adb801
+	github.com/containerd/cgroups v1.1.0
+	github.com/cri-o/cpuset v0.0.1
+	github.com/prometheus/client_golang v1.15.1
+	github.com/prometheus/procfs v0.9.0
+)
+
+require (
+	github.com/Masterminds/semver v1.5.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bits-and-blooms/bitset v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/containerd/cgroups v1.1.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
-	github.com/cri-o/cpuset v0.0.1 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/go-logr/logr v0.4.0 // indirect
 	github.com/godbus/dbus/v5 v5.0.4 // indirect
@@ -15,10 +23,8 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/opencontainers/runtime-spec v1.0.2 // indirect
-	github.com/prometheus/client_golang v1.15.1 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
-	github.com/prometheus/procfs v0.9.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	k8s.io/klog/v2 v2.8.0 // indirect
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/NVIDIA/go-dcgm/pkg/dcgm"
 	"github.com/containerd/cgroups"
 	"github.com/cri-o/cpuset"
 	"github.com/prometheus/client_golang/prometheus"
@@ -17,7 +18,10 @@ import (
 	"github.com/prometheus/procfs"
 )
 
-type cgroupCollector struct {
+var dcgm_enabled bool
+var nvml_enable bool
+
+type jobCollector struct {
 	slurm_job_memory_usage         *prometheus.Desc
 	slurm_job_memory_max           *prometheus.Desc
 	slurm_job_memory_limit         *prometheus.Desc
@@ -31,10 +35,11 @@ type cgroupCollector struct {
 	slurm_job_core_usage           *prometheus.Desc
 	slurm_job_process_count        *prometheus.Desc
 	slurm_job_threads_count        *prometheus.Desc
+	slurm_job_memory_usage_gpu     *prometheus.Desc
 }
 
-func newCgroupCollector() *cgroupCollector {
-	return &cgroupCollector{
+func newJobCollector() *jobCollector {
+	return &jobCollector{
 		slurm_job_memory_usage: prometheus.NewDesc("slurm_job_memory_usage",
 			"Memory used by a job",
 			[]string{"account", "slurmjobid", "user"}, nil,
@@ -87,10 +92,14 @@ func newCgroupCollector() *cgroupCollector {
 			"Number of threads in a job",
 			[]string{"account", "slurmjobid", "user", "state"}, nil,
 		),
+		slurm_job_memory_usage_gpu: prometheus.NewDesc("slurm_job_memory_usage_gpu",
+			"Memory used by a job on a GPU",
+			[]string{"account", "slurmjobid", "user"}, nil,
+		),
 	}
 }
 
-func (collector *cgroupCollector) Describe(ch chan<- *prometheus.Desc) {
+func (collector *jobCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.slurm_job_memory_usage
 	ch <- collector.slurm_job_memory_max
 	ch <- collector.slurm_job_memory_limit
@@ -104,7 +113,7 @@ func (collector *cgroupCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.slurm_job_core_usage
 }
 
-func (collector *cgroupCollector) Collect(ch chan<- prometheus.Metric) {
+func (collector *jobCollector) Collect(ch chan<- prometheus.Metric) {
 	files, _ := filepath.Glob("/sys/fs/cgroup/memory/slurm/uid_*")
 	for _, file := range files {
 		uid := filepath.Base(file)[4:]
@@ -112,6 +121,7 @@ func (collector *cgroupCollector) Collect(ch chan<- prometheus.Metric) {
 
 		job_paths, _ := filepath.Glob("/sys/fs/cgroup/memory/slurm/uid_" + uid + "/job_*")
 		for _, job_path := range job_paths {
+			// for each job on the node
 			job_id := filepath.Base(job_path)[4:]
 
 			control, err := cgroups.Load(cgroups.V1, cgroups.StaticPath("/slurm/uid_"+uid+"/job_"+job_id))
@@ -216,6 +226,53 @@ func (collector *cgroupCollector) Collect(ch chan<- prometheus.Metric) {
 					float64(stats.CgroupStats.NrIoWait),
 					account, job_id, user.Username, "disk-sleep")
 			}
+
+			if dcgm_enabled {
+				fields := []dcgm.Short{
+					dcgm.DCGM_FI_PROF_GR_ENGINE_ACTIVE,        //float64
+					dcgm.DCGM_FI_PROF_SM_ACTIVE,               //float64
+					dcgm.DCGM_FI_PROF_SM_OCCUPANCY,            //float64
+					dcgm.DCGM_FI_PROF_PIPE_TENSOR_ACTIVE,      //float64
+					dcgm.DCGM_FI_PROF_DRAM_ACTIVE,             //float64
+					dcgm.DCGM_FI_PROF_PIPE_FP64_ACTIVE,        //float64
+					dcgm.DCGM_FI_PROF_PIPE_FP32_ACTIVE,        //float64
+					dcgm.DCGM_FI_PROF_PIPE_FP16_ACTIVE,        //float64
+					dcgm.DCGM_FI_PROF_PCIE_TX_BYTES,           //int64
+					dcgm.DCGM_FI_PROF_PCIE_RX_BYTES,           //int64
+					dcgm.DCGM_FI_PROF_NVLINK_TX_BYTES,         //int64
+					dcgm.DCGM_FI_PROF_NVLINK_RX_BYTES,         //int64
+					dcgm.DCGM_FI_PROF_PIPE_TENSOR_IMMA_ACTIVE, //int64
+					dcgm.DCGM_FI_PROF_PIPE_TENSOR_HMMA_ACTIVE, //int64
+					dcgm.DCGM_FI_PROF_PIPE_TENSOR_DFMA_ACTIVE, //int64
+					dcgm.DCGM_FI_PROF_PIPE_INT_ACTIVE,         //int64
+				}
+
+				//var fieldsGroup dcgm.FieldHandle
+				//dcgm.WatchFields(0, fieldsGroup, "slurm-job")
+				//dcgm.UpdateAllFields()
+
+				var values []dcgm.FieldValue_v1
+				values, err = dcgm.GetLatestValuesForFields(0, fields) // do not return the correct values
+				// since dcgm-exporter does not use this function, it might be a bug in the library
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, value := range values {
+					switch value.FieldType {
+					case dcgm.DCGM_FT_DOUBLE:
+						v := value.Float64()
+						log.Println("DCGM latest value float:", value.FieldId, v)
+					case dcgm.DCGM_FT_INT64:
+						v := value.Int64()
+						log.Println("DCGM latest value int:", value.FieldId, v)
+					case dcgm.DCGM_FT_STRING:
+						v := value.String()
+						log.Println("DCGM latest value string:", value.FieldId, v)
+					default:
+						log.Fatal("DCGM failed to convert value")
+					}
+				}
+			}
 		}
 	}
 }
@@ -223,8 +280,21 @@ func (collector *cgroupCollector) Collect(ch chan<- prometheus.Metric) {
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	r := prometheus.NewRegistry()
-	cgroups := newCgroupCollector()
-	r.MustRegister(cgroups)
+	job_collector := newJobCollector()
+	r.MustRegister(job_collector)
+
+	cleanup, err := dcgm.Init(dcgm.Standalone, "localhost:5555", "0")
+	defer cleanup()
+	if err != nil {
+		log.Fatal(err)
+		log.Println("DCGM disabled")
+		dcgm_enabled = false
+	} else {
+		log.Println("DCGM initialized")
+		dcgm_enabled = true
+	}
+	nvml_enable = false //TODO as a fallback if dcgm is not available
+
 	handler := promhttp.HandlerFor(r, promhttp.HandlerOpts{})
 	http.Handle("/metrics", handler)
 	http.ListenAndServe(":9101", nil)
